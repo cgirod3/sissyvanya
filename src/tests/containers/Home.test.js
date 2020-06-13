8510ba5dd0be0dea820b72d407c61eb3b2a880bf
cgirod3 <carlosegirod@gmail.com>
2020-06-13 13:46:29 -0500

    update tests

--- src/tests/containers/Home.test.js
+++ src/tests/containers/Home.test.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
+import BodyCssClass from '../../components/BodyCssClass';
 import Home from '../../containers/Home';
 
-it('renders div and text', () => {
+it('renders div and body', () => {
   const wrapper = shallow(<Home />);
   expect(wrapper.find('div').length).toEqual(1);
-  const div = wrapper.find('div').get(0);
-  expect(div.props.children).toEqual('Home');
+  expect(wrapper.find(BodyCssClass).length).toEqual(1);
+  const body = wrapper.find(BodyCssClass).get(0);
+  expect(body.props.cssClass).toEqual('Home-body');
 });
